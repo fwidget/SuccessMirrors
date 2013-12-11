511ada139cdf7402871ad8bd5799d42f5b974ca7
KIMSEONGTAN <kimseongtan@KIMSEONGTAN-no-MacBook-Air.local>
2013-12-11 23:49:34 +0900

    Initial Commit

---
+++ SuccessMirror/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SuccessMirror
+//
+//  Created by KIMSEONGTAN on 2013/12/11.
+//  Copyright (c) 2013年 successmirror. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SuccessMirror/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuccessMirror
+//
+//  Created by KIMSEONGTAN on 2013/12/11.
+//  Copyright (c) 2013年 successmirror. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
